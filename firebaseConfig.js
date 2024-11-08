c465fee6a4c363bafb9d2c6dbd3d8c93ec3bdd60
Terhi <125193156+TerhiPine@users.noreply.github.com>
2024-11-08 10:57:49 +0200

    firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVFrGVM0KvcPVUPHM-ptsPvn9Lf2qw6yA",
+  authDomain: "labwork3-64a21.firebaseapp.com",
+  projectId: "labwork3-64a21",
+  storageBucket: "labwork3-64a21.firebasestorage.app",
+  messagingSenderId: "763036698503",
+  appId: "1:763036698503:web:b4eacf37ec914747e93522"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
